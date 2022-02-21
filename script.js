2dcb38926b39028b1e2fdbe08c20e2e6f322b8aa
rov9 <duttasaurabh92@gmail.com>
2022-02-21 21:15:00 +0530

    Landing page and avatar ui elements added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+   
+document.getElementById("open-sidebar").onclick = function openNav()
+{
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+
+document.getElementById("close-sidebar").onclick = function closeNav()
+{
+    document.getElementById("mySidenav").style.width = "0";
+}
