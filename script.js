9c07c6a7850a3ce67058d90019cdeb1cfa2f141d
rov9 <duttasaurabh92@gmail.com>
2022-02-22 08:48:21 +0530

    Alert,avatar,slider,toast elements added and updated acc to previous p.r

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ document.getElementById("close-sidebar").onclick = function closeNav()
 {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+document.getElementById("snackbar-btn").onclick = function openSnackbar(){
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
